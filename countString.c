7f4974d1671f8f293e8c4c9fdac10a55a25a0442
StanislavRudenkoko <stanislavrudenkoko@gmail.com>
2025-11-24 17:53:54 -0800

    simplify the C version for demonstration

--- countString.c
+++ countString.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-
-int count_chars(const char *str) {
-    int count = 0;
-
-    while (*str != '\0') {   // loop until null terminator
-        count++;             // increase counter
-        str++;               // move to next character
-    }
-
-    return count;
-}
+#include <string.h>
 
 int main() {
     char text[] = "Hello world!";
-    int length = count_chars(text);
+    int length = strlen(text);
 
-    printf("Length = %d\n", length);
+    printf("%d\n", length);
+
     return 0;
 }
